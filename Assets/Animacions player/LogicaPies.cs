2b0f9b8ad2ed01df9ee82c3876d1c4227c8ed20f
Daniel Gubianas <daniel.gubianas@estudiantat.upc.edu>
2024-12-02 20:57:58 +0100

    Movimiento personaje

---
+++ Assets/Animacions player/LogicaPies.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public MovimentPersoanje Mp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Mp.puedoSaltar = true;
+    }
+    private void OnTriggerExit (Collider other)
+    {
+        Mp.puedoSaltar = false;
+    }
+}
